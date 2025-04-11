2f8fd5bdf204eef8717cd5ac563cac7c3f76e957
AaryanSuwal <aaryansuwal1@gmail.com>
2025-04-11 08:22:24 +0545

    about python dictionary

---
+++ dictionary.py
@@ -0,0 +1,31 @@
+dictionary = {
+    "brand" : "Toyota",
+    "model" : "Hilux",
+    "year" : 2007,
+    "level" : ["Low level", "Middle level", "Top level"]
+}
+
+print(dictionary)                     #prints whole dictionary
+print(dictionary["year"])             #prints the value of certain detail in the dictionary 
+print(dictionary["level"][2])         #print the value in the list
+
+
+
+
+player = {
+    "name" :"messi",
+    "Country" : "Argentina",
+    "age" : 37,
+    "clubs" : ["Barcelona", "PSG", "LA"]
+}
+
+print(player)
+print(player["Country"])
+print(player["clubs"][1])
+
+
+string1 = "We are \t class"
+print(string1)      #\t give space between data
+
+string2 = "We are \n class"
+print(string2)      #\n print remaning data in new line
