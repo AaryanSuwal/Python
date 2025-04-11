51a5ed5e53ca1e29c0b66197857ca08235774151
AaryanSuwal <aaryansuwal1@gmail.com>
2025-04-11 08:23:49 +0545

    about python string slicing

---
+++ slicing.py
@@ -0,0 +1,29 @@
+# list="My name is Aaryan Suwal"
+# print(list[0:4]) # My n
+
+# name = "This is Python Programming"
+# length = len(name)
+# print(length)
+# print(name[3:15])
+# print(name[:])
+# print(name[2:7])
+# print(name[2:])
+# print(name[3:3])
+# print(name[3:4])
+# print(name[3:22:2]) # 2 will be the step size
+
+# print(name[::-1]) # Reverse the string
+# print(name[22:3:-1])
+# print(name[-3:-15:-1])
+
+
+# Python program to check if a string is palindrome or not
+# list = "racecar"
+# list_1 = list[::-1]
+# if list == list_1:
+#     print("The string is palindrome")
+# else:
+#     print("The string is not palindrome")
+
+# a = 12345
+# print(a[::-1]) # This will give an error because int is not subscriptable
