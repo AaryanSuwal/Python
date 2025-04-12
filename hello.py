ce570a0063d1b8216df78427918fc91cf6de5234
AaryanSuwal <aaryansuwal1@gmail.com>
2025-04-12 10:43:02 +0545

    Learning python

---
+++ hello.py
@@ -0,0 +1,18 @@
+# Python Data Types
+# String
+name = "hello"
+lname = "world"
+print("My name is", name,"Last Name is", lname)
+print(len(name))
+print(len(lname))
+
+# Boolean
+# True or False
+x = 12
+y = 5
+print(bool(x > y))
+print(bool(x < y))
+
+# String Concatenation
+name = "Kathmandu"
+print(name)
