ce4732be817610b1443b102ab36b10dcb5d3b263
AaryanSuwal <aaryansuwal1@gmail.com>
2025-04-21 09:42:42 +0545

    About Python User Define function

---
+++ userDefine.py
@@ -0,0 +1,12 @@
+print("Hello, World!!")
+def Hi():
+    print("Hello, how are you?")
+Hi()
+
+def sum():
+    num1 = int(input("Enter a Number:"))
+    num2 = int(input("Enter another number:"))
+
+    sum1 = num1 + num2
+    print(sum1)
+sum()
